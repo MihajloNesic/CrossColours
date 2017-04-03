933a063df03fafa9c3946b7e9312c4a791c0ef01
Mihajlo <nesic.srbija98@gmail.com>
2017-04-03 14:39:19 +0100

    Initial commit

---
+++ CrossColoursVB/Main.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.tlp_game = New System.Windows.Forms.TableLayoutPanel()
+        Me.btn_reset = New System.Windows.Forms.Button()
+        Me.nud_rows = New System.Windows.Forms.NumericUpDown()
+        Me.nud_cols = New System.Windows.Forms.NumericUpDown()
+        Me.btn_generate = New System.Windows.Forms.Button()
+        Me.pcb_logo = New System.Windows.Forms.PictureBox()
+        CType(Me.nud_rows, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nud_cols, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pcb_logo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'tlp_game
+        '
+        Me.tlp_game.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tlp_game.ColumnCount = 6
+        Me.tlp_game.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.tlp_game.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.tlp_game.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.tlp_game.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.tlp_game.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.tlp_game.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.tlp_game.Location = New System.Drawing.Point(12, 69)
+        Me.tlp_game.Name = "tlp_game"
+        Me.tlp_game.RowCount = 6
+        Me.tlp_game.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.tlp_game.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.tlp_game.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.tlp_game.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.tlp_game.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.tlp_game.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.tlp_game.Size = New System.Drawing.Size(460, 438)
+        Me.tlp_game.TabIndex = 0
+        '
+        'btn_reset
+        '
+        Me.btn_reset.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btn_reset.Location = New System.Drawing.Point(16, 513)
+        Me.btn_reset.Name = "btn_reset"
+        Me.btn_reset.Size = New System.Drawing.Size(75, 23)
+        Me.btn_reset.TabIndex = 1
+        Me.btn_reset.Text = "Reset"
+        Me.btn_reset.UseVisualStyleBackColor = True
+        '
+        'nud_rows
+        '
+        Me.nud_rows.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.nud_rows.Location = New System.Drawing.Point(263, 516)
+        Me.nud_rows.Maximum = New Decimal(New Integer() {9, 0, 0, 0})
+        Me.nud_rows.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.nud_rows.Name = "nud_rows"
+        Me.nud_rows.Size = New System.Drawing.Size(58, 20)
+        Me.nud_rows.TabIndex = 2
+        Me.nud_rows.Value = New Decimal(New Integer() {6, 0, 0, 0})
+        '
+        'nud_cols
+        '
+        Me.nud_cols.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.nud_cols.Location = New System.Drawing.Point(327, 516)
+        Me.nud_cols.Maximum = New Decimal(New Integer() {9, 0, 0, 0})
+        Me.nud_cols.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.nud_cols.Name = "nud_cols"
+        Me.nud_cols.Size = New System.Drawing.Size(58, 20)
+        Me.nud_cols.TabIndex = 3
+        Me.nud_cols.Value = New Decimal(New Integer() {6, 0, 0, 0})
+        '
+        'btn_generate
+        '
+        Me.btn_generate.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_generate.Location = New System.Drawing.Point(391, 513)
+        Me.btn_generate.Name = "btn_generate"
+        Me.btn_generate.Size = New System.Drawing.Size(75, 23)
+        Me.btn_generate.TabIndex = 4
+        Me.btn_generate.Text = "Generate"
+        Me.btn_generate.UseVisualStyleBackColor = True
+        '
+        'pcb_logo
+        '
+        Me.pcb_logo.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pcb_logo.Image = Global.CrossColoursVB.My.Resources.Resources.brainbashers
+        Me.pcb_logo.Location = New System.Drawing.Point(12, 2)
+        Me.pcb_logo.Name = "pcb_logo"
+        Me.pcb_logo.Size = New System.Drawing.Size(460, 61)
+        Me.pcb_logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pcb_logo.TabIndex = 5
+        Me.pcb_logo.TabStop = False
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 548)
+        Me.Controls.Add(Me.pcb_logo)
+        Me.Controls.Add(Me.btn_generate)
+        Me.Controls.Add(Me.nud_cols)
+        Me.Controls.Add(Me.nud_rows)
+        Me.Controls.Add(Me.btn_reset)
+        Me.Controls.Add(Me.tlp_game)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MinimumSize = New System.Drawing.Size(334, 405)
+        Me.Name = "Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Cross Colours VB"
+        CType(Me.nud_rows, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nud_cols, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pcb_logo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents tlp_game As TableLayoutPanel
+    Friend WithEvents btn_reset As Button
+    Friend WithEvents nud_rows As NumericUpDown
+    Friend WithEvents nud_cols As NumericUpDown
+    Friend WithEvents btn_generate As Button
+    Friend WithEvents pcb_logo As PictureBox
+End Class
---
+++ CrossColoursVB/Main.vb
@@ -0,0 +1,110 @@
+﻿Public Class Main
+
+    Private CCGreen As Color = Color.FromArgb(255, 51, 255, 0)
+
+    Private ButtonMatrix(,) As Button
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CreateGame(6, 6)
+    End Sub
+
+    Private Sub btn_reset_Click(sender As Object, e As EventArgs) Handles btn_reset.Click
+        For i As Integer = 0 To tlp_game.RowCount - 1
+            For j As Integer = 0 To tlp_game.ColumnCount - 1
+                ButtonMatrix(i, j).BackColor = Color.Red
+            Next
+        Next
+    End Sub
+
+    Private Sub btn_generate_Click(sender As Object, e As EventArgs) Handles btn_generate.Click
+        Dim r As Integer = Convert.ToInt32(nud_rows.Value)
+        Dim c As Integer = Convert.ToInt32(nud_cols.Value)
+        CreateGame(r, c)
+    End Sub
+
+    Private Sub pcb_logo_Click(sender As Object, e As EventArgs) Handles pcb_logo.Click
+        System.Diagnostics.Process.Start("https://www.brainbashers.com/crosscolours.asp")
+        System.Diagnostics.Process.Start("https://mihajlonesic.gitlab.io/")
+    End Sub
+
+    Private Sub CreateGame(ByVal row As Integer, ByVal col As Integer)
+        tlp_game.Controls.Clear()
+
+        tlp_game.RowStyles.Clear()
+        tlp_game.ColumnStyles.Clear()
+
+        tlp_game.RowCount = row
+        tlp_game.ColumnCount = col
+
+        ButtonMatrix = New Button(row - 1, col - 1) {}
+
+        For i As Integer = 0 To row - 1
+            For j As Integer = 0 To col - 1
+                ButtonMatrix(i, j) = New Button()
+                ButtonMatrix(i, j).BackColor = Color.Red
+                ButtonMatrix(i, j).Padding = New Padding(0)
+                ButtonMatrix(i, j).Margin = New Padding(0)
+                ButtonMatrix(i, j).Anchor = (AnchorStyles.Bottom Or AnchorStyles.Top Or AnchorStyles.Right Or AnchorStyles.Left)
+                AddHandler ButtonMatrix(i, j).Click, AddressOf CCButtonClickHandler
+                tlp_game.Controls.Add(ButtonMatrix(i, j), j, i)
+            Next
+        Next
+
+        For i As Integer = 1 To tlp_game.RowCount
+            tlp_game.RowStyles.Add(New RowStyle(SizeType.Percent, 1))
+        Next
+
+        For i As Integer = 1 To tlp_game.ColumnCount
+            tlp_game.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 1))
+        Next
+    End Sub
+
+    Private Sub CCButtonClickHandler(ByVal sender As Object, ByVal e As EventArgs)
+        Dim b As Button = CType(sender, Button)
+
+        If b IsNot Nothing Then
+            ToggleColor(b)
+
+            Dim pos As TableLayoutPanelCellPosition = tlp_game.GetCellPosition(b)
+
+            If pos.Row < (tlp_game.RowCount) Then
+                Dim neig1 As Button = CType(tlp_game.GetControlFromPosition(pos.Column, pos.Row + 1), Button)
+                If neig1 IsNot Nothing Then ToggleColor(neig1)
+
+                If pos.Row > 0 Then
+                    Dim neig2 As Button = CType(tlp_game.GetControlFromPosition(pos.Column, pos.Row - 1), Button)
+                    If neig2 IsNot Nothing Then ToggleColor(neig2)
+                End If
+            End If
+
+            If pos.Column < (tlp_game.ColumnCount) Then
+                Dim neig3 As Button = CType(tlp_game.GetControlFromPosition(pos.Column + 1, pos.Row), Button)
+                If neig3 IsNot Nothing Then ToggleColor(neig3)
+
+                If pos.Column > 0 Then
+                    Dim neig4 As Button = CType(tlp_game.GetControlFromPosition(pos.Column - 1, pos.Row), Button)
+                    If neig4 IsNot Nothing Then ToggleColor(neig4)
+                End If
+            End If
+        End If
+
+        If Win() Then MessageBox.Show("You are victorious!", "Win", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub ToggleColor(ByVal b As Button)
+        If b.BackColor = Color.Red Then
+            b.BackColor = CCGreen
+        Else
+            b.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Function Win() As Boolean
+        For i As Integer = 0 To tlp_game.RowCount - 1
+            For j As Integer = 0 To tlp_game.ColumnCount - 1
+                If ButtonMatrix(i, j).BackColor <> CCGreen Then Return False
+            Next
+        Next
+        Return True
+    End Function
+End Class
